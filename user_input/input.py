2dc31a4a93512232faf60de39ad018b18db3352c
Shani Patel <shanipatel37780@gmail.com>
2026-03-25 22:26:05 +0530

    user_input
    user input in python

---
+++ user_input/input.py
@@ -0,0 +1,12 @@
+
+#Example1 Symple User Input
+# name = input("Enter Your Name:")
+# print("hello", name)
+
+
+#Example2 User Input With Number
+num1 = int(input("Enter your First Number:"))
+num2 = int(input("Enter Your Sencond Number:"))
+sum =num1 +num2
+print("Sum of Number = ", sum)
+
