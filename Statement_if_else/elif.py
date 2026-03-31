a503f5c21ab69ee6f3905c94e4bf1c1d34161d04
Shani Patel <shanipatel37780@gmail.com>
2026-03-31 20:48:59 +0530

    Statement_if_else

---
+++ Statement_if_else/elif.py
@@ -0,0 +1,17 @@
+# marks = 75
+# if marks >= 60:
+#     print("Grade A")
+#     elif marks >= 60:
+#         print("Grade B")
+#         else:
+#             print("Grade C")
+
+
+marks = 75
+
+if marks >= 60:
+    print("Grade A")
+elif marks >= 50:
+    print("Grade B")
+else:
+    print("Grade C")
